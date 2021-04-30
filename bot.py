9f03fec1c157df2f0d28f6967e290f7be165029f
Pop101 <leon.leibmann@gmail.com>
2021-04-29 19:52:34 -0700

    Use Embeds

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from Levenshtein import distance
 from whats_that_code.election import guess_language_all_methods # Guesslang does not run for me
-import re, requests
+import re, requests, time
 import boilerplates
 
 import yaml
@@ -27,8 +27,18 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    code_blocks = map(lambda x: x.group(), re.finditer(r'(```.*\n).*?(\n```)', message.content, flags=re.M|re.S))
-    
+    # Quick scan for at least 1 match
+    if not re.match(r'(```.*?\n).*?(\n```)', message.content, flags=re.M|re.S): return
+
+    # Get all valid languages and their versions
+    versions = dict()
+    for lang in requests.get('https://emkc.org/api/v2/piston/runtimes').json():
+        versions[lang['language']] = lang['version']
+        for alias in lang['aliases']: versions[alias] = lang['version']
+
+    # Parse all code blocks into code of a specific language
+    code_blocks = map(lambda x: x.group(), re.finditer(r'(```.*?\n).*?(\n```)', message.content, flags=re.M|re.S))
+    code_to_run = list()
     for code_block in code_blocks:
         # Parse lang and code out of the block
         language = code_block.split('\n')[0][3:].strip()
@@ -39,30 +49,41 @@ async def on_message(message):
             language = guess_language_all_methods(code)
         
         # Get closest lang
-        versions = dict()
-        for lang in requests.get('https://emkc.org/api/v2/piston/runtimes').json():
-            versions[lang['language']] = lang['version']
-            for alias in lang['aliases']: versions[alias] = lang['version']
-        
         language = min(versions.keys(), key=lambda x: distance(language, x) / len(language))
         
         if language in boilerplates.supported_languages:
             code = boilerplates.add_boilerplate(language, code)
-        
-        # Execute code using piston api
+        code_to_run.append((language, code))
+
+    # Execute code using piston api
+    embed = discord.Embed(title='Code Ran', colour=discord.Colour.green())
+    for i, language, code in zip(range(1, len(code_to_run)+1), *zip(*code_to_run)):
+        title = f'Code Block{f" {i}" if len(code_to_run) > 1 else ""}'
         req = requests.post('https://emkc.org/api/v2/piston/execute', json={
             'language': language,
             'version': versions[language],
             'files': [
-                {'name': f'temp.txt', 'content': code}
+                {'name': title, 'content': code}
             ]
         })
+        
         output = req.json()
-
-        # get output or stdout
+        
+        # Get output
         if 'run' not in output: continue
-        output = (output['run']['output'] or output['run']['stdout'] or '').strip()
-        if output: await message.channel.send(output)
+
+        emoji = '✅' if not output['run']['stderr'] else '❎'
+        output = output['run']['output'].strip()
+
+        # Add output to embed
+        if len(code_to_run) > 1:
+            embed.add_field(name=f'{emoji} {title} {emoji}', value=output, inline=False)
+            if i < len(code_to_run): time.sleep(0.5) # sleep through ratelimit
+        else:
+            embed.title = f'{emoji} {title} {emoji}'
+            embed.description = output
+    
+    await message.reply(embed=embed, mention_author=False)
 
 bot.run(config['token'])
 print('\n\nShut Down')
